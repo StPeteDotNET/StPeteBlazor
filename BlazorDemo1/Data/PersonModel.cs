13d0bff4370513de1d07d2bde8c6837fc184b916
Tim Corey <tim@iamtimcorey.com>
2020-08-11 19:12:08 -0500

    After the Meetup

---
+++ BlazorDemo1/Data/PersonModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorDemo1.Data
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
